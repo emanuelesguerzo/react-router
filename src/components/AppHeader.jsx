eba757853439aa2a6ed02379256170f30411a446
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-01-09 14:52:35 +0100

    feat: MainNav added and initial styling

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,10 @@
+import MainNav from "./MainNav";
+
 const AppHeader = () => {
     return (
         <header>
             <h1>React Form</h1>
+            <MainNav />
         </header>
     )
 }
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,4 +1,32 @@
+import { NavLink } from "react-router-dom";
+
 function MainNav() {
+    const paths = [
+        {
+            path: "/",
+            title: "Home",
+        },
+        {
+            path: "/posts",
+            title: "Posts",
+        },
+        {
+            path: "/about",
+            title: "About",
+        },
+    ]
+
+    return (
+        <nav>
+            <ul>
+                {paths.map((curItem) => (
+                    <li>
+                        <NavLink to={curItem.path}>{curItem.title}</NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 
 }
 
